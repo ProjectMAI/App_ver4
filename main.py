ebc2176edddd21eabf1ccc0cd681910f8978925e
ProjectMAI <rodionov.mihail02@yandex.ru>
2021-10-31 17:03:15 +0300

    added main default screen, fix some labels

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from kivy.app import App
+from kivy.lang import Builder
 
 from kivy.core.window import Window
 
 from kivy.properties import ObjectProperty
+
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 Window.size = (360, 640)
+
 
 class RegistrationScreen(Screen):
     namee = ObjectProperty(None)
@@ -17,18 +20,32 @@ class RegistrationScreen(Screen):
 class SignUpScreen(Screen):
     pass
 
+
 class MainScreen(Screen):
     pass
 
 class ProfileScreen(Screen):
     pass
 
+
 class WindowManager(ScreenManager):
     pass
 
 
+kv = Builder.load_file("project.kv")
+
+sm = WindowManager()
+
+screens = [SignUpScreen(name="sign"), RegistrationScreen(name="registration"), MainScreen(name="main"), ProfileScreen(name="profile")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "sign"
+
+
 class ProjectApp(App):
-    pass
+    def build(self):
+        return sm
 
 if __name__ == "__main__":
     ProjectApp().run()
